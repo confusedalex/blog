b68afaaf74849d3079f8bda23fed6ec63bcf76d6
confusedalex <alex@confusedalex.dev>
2025-01-03 13:15:50 +0100

    feat: add fetch-webmentions package to flake.nix

--- flake.nix
+++ flake.nix
@@ -5,15 +5,28 @@
   };
 
   outputs =
-    { systems, nixpkgs, ... }@inputs:
+    { self, systems, nixpkgs, ... }@inputs:
     let
       eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
     in
     {
+      packages = eachSystem (pkgs: {
+        fetch-webmentions = pkgs.writeShellApplication {
+          name = "fetch-webmentions";
+          runtimeInputs = with pkgs; [
+            curl
+            jq
+          ];
+          text = (builtins.readFile ./fetch-webmentions.sh);
+        };
+      });
+
       devShells = eachSystem (pkgs: {
         default = pkgs.mkShell {
           buildInputs = with pkgs; [
             zola
+            jq
+            self.packages.${system}.fetch-webmentions
           ];
         };
       });
